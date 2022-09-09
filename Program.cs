59b10d0af86aa53d542ae6be6a8cf363f6850fd8
JavierABH <javierfchernandez10@gmail.com>
2022-09-09 15:14:58 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mover_varios_archivos_a_uno_solo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
